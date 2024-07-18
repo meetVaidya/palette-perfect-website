a51d9f0f4201094fb8a67beea7614d3f301dcd61
meetVaidya <vaidyameet2004@gmail.com>
2024-07-18 18:14:20 +0530

    chore: Update supabase.js to use environment variables

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,6 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+let supabase;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+if (typeof window !== 'undefined') {
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+    if (!supabaseUrl || !supabaseAnonKey) {
+        throw new Error('Missing Supabase environment variables');
+    }
+
+    supabase = createClient(supabaseUrl, supabaseAnonKey);
+}
+
+export { supabase };
